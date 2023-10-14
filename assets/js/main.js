54c2631fb9ff8651aa92faab677752ff72a5f630
YashJoshi2109 <yashjosh7486@gmail.com>
2023-10-14 10:34:50 +0530

    service card container

--- assets/js/main.js
+++ assets/js/main.js
@@ -405,3 +405,25 @@ timelineItems.forEach((item) => {
     item.classList.add("selected");
   });
 });
+
+// services script
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollRight = document.getElementById("scrollRight");
+  const productListContainer = document.querySelector(
+    ".product-list-container"
+  );
+
+  console.log("Initial Scroll Left:", productListContainer.scrollLeft);
+
+  scrollRight.addEventListener("click", function () {
+    console.log("Clicked!");
+    productListContainer.scrollBy({
+      top: 0,
+      left: 620,
+      behavior: "smooth",
+    });
+    console.log("Scroll Left After:", productListContainer.scrollLeft);
+  });
+});
