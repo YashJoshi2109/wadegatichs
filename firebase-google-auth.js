fe585c853d95106f7ac7918f015467a6b1fc1c6f
YashJoshi2109 <yashjosh7486@gmail.com>
2023-10-21 14:38:53 +0530

    LOGIN MODULE

--- firebase-google-auth.js
+++ firebase-google-auth.js
@@ -1,55 +1,53 @@
-// Replace with your Google Cloud Console API client IDII
-const clientId =
-  "367081023101-20oa44onra18ru34lg5of2dqkvco32j0.apps.googleusercontent.com"; // Replace with your actual client ID
-const allowedEmails = [
-  "yashjosh7486@gmail.com",
-  "yash212479@gmail.com.com" /* Add your 28 allowed emails here */,
+const themes = [
+  {
+    background: "#1A1A2E",
+    color: "#FFFFFF",
+    primaryColor: "#0F3460",
+  },
+  {
+    background: "#461220",
+    color: "#FFFFFF",
+    primaryColor: "#E94560",
+  },
+  {
+    background: "#192A51",
+    color: "#FFFFFF",
+    primaryColor: "#967AA1",
+  },
+  {
+    background: "#F7B267",
+    color: "#000000",
+    primaryColor: "#F4845F",
+  },
+  {
+    background: "#F25F5C",
+    color: "#000000",
+    primaryColor: "#642B36",
+  },
+  {
+    background: "#231F20",
+    color: "#FFF",
+    primaryColor: "#BB4430",
+  },
 ];
-const scope = "https://www.googleapis.com/auth/userinfo.profile"; // Specify the scope of access
 
-// Load the Google Sign-In API
-gapi.load("auth2", function () {
-  gapi.auth2.init({
-    client_id: clientId,
+const setTheme = (theme) => {
+  const root = document.querySelector(":root");
+  root.style.setProperty("--background", theme.background);
+  root.style.setProperty("--color", theme.color);
+  root.style.setProperty("--primary-color", theme.primaryColor);
+  root.style.setProperty("--glass-color", theme.glassColor);
+};
+
+const displayThemeButtons = () => {
+  const btnContainer = document.querySelector(".theme-btn-container");
+  themes.forEach((theme) => {
+    const div = document.createElement("div");
+    div.className = "theme-btn";
+    div.style.cssText = `background: ${theme.background}; width: 25px; height: 25px`;
+    btnContainer.appendChild(div);
+    div.addEventListener("click", () => setTheme(theme));
   });
-});
+};
 
-// Sign in with Google
-function signInWithGoogle() {
-  gapi.auth2
-    .getAuthInstance()
-    .signIn()
-    .then(function (googleUser) {
-      const profile = googleUser.getBasicProfile();
-      const userEmail = profile.getEmail();
-      if (allowedEmails.includes(userEmail)) {
-        // User's email is in the allowed list - access granted.
-        document.getElementById("message").textContent =
-          "Access granted. Redirecting...";
-        alert("successfully done");
-        // Perform a redirect to the desired page.
-        window.location.href = "blog.html";
-      } else {
-        // User's email is not in the allowed list - access denied.
-        document.getElementById("message").textContent =
-          "Your email is not registered. Contact administrator for inquiries.";
-        signOutGoogleUser();
-      }
-    });
-}
-
-// Sign out the user
-function signOutGoogleUser() {
-  gapi.auth2
-    .getAuthInstance()
-    .signOut()
-    .then(function () {
-      alert("Signout success");
-      console.log("User signed out.");
-    });
-}
-
-// Attach click event to the "Sign in with Google" button
-document
-  .getElementById("google-signin-button")
-  .addEventListener("click", signInWithGoogle);
+displayThemeButtons();
