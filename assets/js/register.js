896b59d3439016041e9e5f1976b7e2fd56fcdb8c
YashJoshi2109 <yashjosh7486@gmail.com>
2023-10-22 15:10:09 +0530

    changes

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const loginForm = document.getElementById("login-form");
+  const loginForm = document.getElementById("register-form");
 
   loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -8,11 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const name = document.getElementById("name").value;
 
     // You can add your authentication logic here.
+    Emaillist = ["yashjosh7486@gmail.com", "yash212479@gmail.com"];
     // Example:
-    if (email === "wadegati" && name === "wadegati") {
+    if (Emaillist.includes(email)) {
       alert("Login successful!");
       // Redirect to another page
-      window.location.href = "blog.html";
+      //   window.location.href = "blog.html";
       // Redirect to another page or perform further actions.
     } else {
       alert("Invalid credentials. Please try again.");
