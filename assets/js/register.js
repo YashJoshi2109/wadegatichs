9687ac64d5196bd5b9e030838b127bea552b9a43
YashJoshi2109 <yashjosh7486@gmail.com>
2023-10-22 18:01:31 +0530

    email done

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,22 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const loginForm = document.getElementById("register-form");
+  const registerForm = document.getElementById("register-form");
 
-  loginForm.addEventListener("submit", function (event) {
+  registerForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const email = document.getElementById("email").value;
     const name = document.getElementById("name").value;
+    const serviceID = "default_service";
+    const templateID = "template_2hf3xob";
 
-    // You can add your authentication logic here.
-    Emaillist = ["yashjosh7486@gmail.com", "yash212479@gmail.com"];
-    // Example:
-    if (Emaillist.includes(email)) {
-      alert("Login successful!");
-      // Redirect to another page
-      //   window.location.href = "blog.html";
-      // Redirect to another page or perform further actions.
+    if (!isValidEmail(email)) {
+      alert("Invalid email format. Please provide a valid email.");
+      return;
+    }
+
+    // List of allowed email addresses
+    const emailList = ["yashjosh7486@gmail.com", "yash212479@gmail.com"];
+
+    if (emailList.includes(email)) {
+      // Send email only if the email is in the list
+      const message = () => {
+        emailjs.send(serviceID, templateID, {
+          from_name: "Wadegati CHS",
+          email: email,
+          name: name,
+        });
+        alert("Check your mail for username & password!");
+        window.location.href = "login.html"; // Redirect to another page
+      };
+
+      message();
     } else {
-      alert("Invalid credentials. Please try again.");
+      alert("Email address not authorized. Please try again.");
     }
   });
 });
+
+function isValidEmail(email) {
+  const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+  return emailRegex.test(email);
+}
